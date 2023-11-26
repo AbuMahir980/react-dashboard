49b07afd84ce79da35a81afd005e9dc359f5e360
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-26 21:20:17 +0100

    Add styling to footer component

--- src/components/Dashboard/components/Footer.jsx
+++ src/components/Dashboard/components/Footer.jsx
@@ -1,9 +1,9 @@
-
+import '../../../css/footer.css'
 const Footer = () => {
     return(
 
         <footer className="footer">
-            <div className="footer_sign">made with &#128053; by <a href="https://github.com/AbuMahir980" target="blank" style={{}}>Dev Abu Mahir</a>. Proudly 🇳🇬</div>
+            <div className="footer_sign">made with &#128053; by <a href="https://github.com/AbuMahir980" target="blank" style={{}}>Abu Mahir</a>. Proudly 🇳🇬</div>
         </footer>
     )
 }
