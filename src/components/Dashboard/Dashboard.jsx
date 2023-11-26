6dcbcd1b17426105e008887a12589a78ffc050ab
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-26 23:51:47 +0100

    Add Dashboard component contents

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import Userprofile from "./datas/Userprofile";
-
+import Chart from '../../assets/dashboard-chart.png';
+import Barchart from '../../assets/dashboard-barchart.png';
+import Graph from '../../assets/dashboard-graph1.png';
+import Piechart from '../../assets/dashboard-piechart.png';
 const Dashboard = (props) => {
 
     const {user} = props
@@ -12,11 +15,23 @@ const Dashboard = (props) => {
             <div id="contents">
                 <Header user={Userprofile}/>
                 <Sidebar />
-                <main>
+                <main className="content">
+                    <div className="content-mini">
+                        <h1>Key Performance Metrics</h1>
                     <div>
-                        <img src="" alt="" />
-                        <h3></h3>
-
+                        <img src={Chart} alt="dashboard-chart" />
+                    </div>
+                    <div className="mini-content">
+                        <div>
+                            <img src={Barchart} alt="dashboard-chart" />
+                        </div>
+                        <div>
+                            <img src={Graph} alt="dashboard-chart" />
+                        </div>
+                        <div>
+                            <img src={Piechart} alt="dashboard-chart" />
+                        </div>
+                    </div>
                     </div>
                 </main>
                 <Footer />
