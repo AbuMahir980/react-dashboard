577b645a4e2922cb6a18ab95948e6d304ddb83ab
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-26 19:48:13 +0100

    Add sidebar styling

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 const Dashboard = () => {
     return(
         <>
             <div id="contents">
                 <Header />
+                <Sidebar />
                 <Footer />
             </div>
         </>
