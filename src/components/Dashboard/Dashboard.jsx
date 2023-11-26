dfe9d98fce54df35706260031a695ee1c96da273
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-26 18:25:01 +0100

    Add footer component contents

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const Dashboard = () => {
     return(
         <>
             <div id="contents">
                 <Header />
+                <Footer />
             </div>
         </>
     )
---
+++ src/components/Dashboard/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return(
+
+        <footer className="footer">
+            <div className="footer_sign">made with &#128053; by <a href="https://github.com/AbuMahir980" target="blank" style={{}}>Dev Abu Mahir</a>. Proudly 🇳🇬</div>
+        </footer>
+    )
+}
+
+export default Footer
