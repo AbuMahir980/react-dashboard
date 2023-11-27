a3b72779ce029fd3cfa72219b80024fa2abb6347
Qudus Lawal <lawalqudus980@gmail.com>
2023-11-27 06:11:57 +0100

    Change contents color choice

--- src/components/Dashboard/Users.jsx
+++ src/components/Dashboard/Users.jsx
@@ -10,49 +10,50 @@ const Users = (props) => {
 
     return(
         <>
-            <div className="contents">
+            <div>
                 <Header user={Userprofile}/>
                 <Sidebar />
-                <main className="content">
-                    <div className="contents">
+                <main className="contents">
+                    <div className="content-min">
                         <img src={HeadShot} alt={`HeadShot of ${user.fullName}`}  style={{width: user.imageSize, height: user.imageSize, borderRadius: 100}}/>
+                        <div className="texts">
+                            <div>
+                                Name:
+                                <h4 className="colors">{user.fullName}</h4>
+                            </div>
+                            <div>
+                                Username:
+                                <h4 className="colors">{user.username}</h4>
+                            </div>
+                            <div>
+                                Email:
+                                <h4 className="colors">{user.email}</h4>
+                            </div>
+                            <div>
+                                Bio:
+                                <h4 className="colors">{user.bio}</h4>
+                            </div>
+                            <div>
+                                Role:
+                                <h4 className="colors">{user.role}</h4>
+                            </div>
+                            <div>
+                                Joined:
+                                <h4 className="colors">{user.joinedAt}</h4>
+                            </div>
+                            <div>
+                                Last Seen:
+                                <h4 className="colors">{user.lastLogin}</h4>
+                            </div>
+                            <div>
+                                Socials:
+                                <h4 className="colors">Twitter: {user.socialLinks.twitter}</h4>
+                                <h4 className="colors">LinkedIn: {user.socialLinks.linkedIn}</h4>
+                                <h4 className="colors">Twitter{user.socialLinks.github}</h4>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <div>
-                            Name:
-                            <h4 className="colors">{user.fullName}</h4>
-                        </div>
-                        <div>
-                            Username:
-                            <h4 className="colors">{user.username}</h4>
-                        </div>
-                        <div>
-                            Email:
-                            <h4 className="colors">{user.email}</h4>
-                        </div>
-                        <div>
-                            Bio:
-                            <h4 className="colors">{user.bio}</h4>
-                        </div>
-                        <div>
-                            Role:
-                            <h4 className="colors">{user.role}</h4>
-                        </div>
-                        <div>
-                            Joined:
-                            <h4 className="colors">{user.joinedAt}</h4>
-                        </div>
-                        <div>
-                            Last Seen:
-                            <h4 className="colors">{user.lastLogin}</h4>
-                        </div>
-                        <div>
-                            Socials:
-                            <h4 className="colors">Twitter: {user.socialLinks.twitter}</h4>
-                            <h4 className="colors">LinkedIn: {user.socialLinks.linkedIn}</h4>
-                            <h4 className="colors">Twitter{user.socialLinks.github}</h4>
-                        </div>
-                    </div>
+                    
                 </main>
                 <Footer />
             </div>
